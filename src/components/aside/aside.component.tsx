4954414183b694197840b95ac941bdab4d7350c7
geepara <62688021+geepara@users.noreply.github.com>
2023-11-15 15:03:21 -0500

    sidebar disappears for small viewing

--- src/components/aside/aside.component.tsx
+++ src/components/aside/aside.component.tsx
@@ -3,13 +3,15 @@ import asideItems from "./asideItems.json";
 
 export default function Aside() {
   return (
-    <nav className="lg:flex lg:flex-col lg:gap-1 lg:p-10 w-[320px] hidden">
+    <nav className="justify-self-end lg:flex lg:flex-col lg:gap-1 lg:p-10 w-[320px] hidden">
       <div className="text-xl">On This Page</div>
       {asideItems.map((item: any) => (
         <Link
           key={item.link}
           href={item.link}
-          className="font-extralight text-sm hover:font-normal hover:transition ease-in-out duration-200"
+          className={
+            "text-sm opacity-60 hover:opacity-90 hover:transition duration-500"
+          }
         >
           {item.name}
         </Link>
--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 export default function Sidebar() {
   const pathname = usePathname();
   return (
-    <nav className="flex flex-col gap-1 p-10 w-[320px]">
+    <nav className="sm:flex sm:flex-col sm:gap-1 sm:p-10 sm:w-[320px] hidden">
       {sections.map((section, index) => (
         <div className="flex flex-col gap-1">
           <div className="text-xl">{section.sectionTitle}</div>
