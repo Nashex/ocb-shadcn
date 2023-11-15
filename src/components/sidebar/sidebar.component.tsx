e1f48b1e7b4ec1b790d1132d3ed17b537da3b4ab
geepara <62688021+geepara@users.noreply.github.com>
2023-11-15 14:56:05 -0500

    active sidebar links

--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -1,21 +1,31 @@
+"use client";
 import Link from "next/link";
-import sidebarItems from "./sidebarItems.json";
+import sections from "./sidebarItems.json";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const pathname = usePathname();
   return (
     <nav className="flex flex-col gap-1 p-10 w-[320px]">
-      {sidebarItems.map((item: any) => (
+      {sections.map((section, index) => (
         <div className="flex flex-col gap-1">
-          <div className="text-lg">{item.title}</div>
-          {item.links.map((link: any) => (
-            <Link
-              key={link.link}
-              href={link.link}
-              className="font-extralight text-sm hover:font-normal hover:transition ease-in-out duration-200"
-            >
-              {link.name}
-            </Link>
-          ))}
+          <div className="text-xl">{section.sectionTitle}</div>
+          {section.links.map((link: any) => {
+            const isActive = pathname === link.link;
+            return (
+              <Link
+                key={link.link}
+                href={link.link}
+                className={
+                  isActive
+                    ? "text-sm font-[700]"
+                    : "text-sm opacity-60 hover:opacity-90 hover:transition duration-500"
+                }
+              >
+                {link.name}
+              </Link>
+            );
+          })}
         </div>
       ))}
     </nav>
