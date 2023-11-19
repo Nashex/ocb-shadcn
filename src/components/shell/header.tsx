a1cd3b655bd4a0a4c6f942155f0ef1a18875f425
nashex <joshnakka1@gmail.com>
2023-11-19 02:55:31 -0500

    Style fix on header padding

--- src/components/shell/header.tsx
+++ src/components/shell/header.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="sticky bg-background z-[10] top-0 h-20 flex flex-row items-center sm:w-fit w-full justify-start p-4 gap-x-2">
+    <header className="sticky bg-background z-[10] top-0 h-20 flex flex-row items-center sm:w-fit w-full justify-start p-4 px-9 gap-x-2">
       <button className="h-10 w-10 border border-border rounded-sm flex items-center justify-center hover:bg-foreground/5">
         <PanelLeftClose size={24} strokeWidth={1.5} />
       </button>
